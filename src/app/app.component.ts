b9dbe7759b08bebacc2a5a1fe89ee77fbeaa264f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-09-13 18:47:53 -0300

    implement switchbox for themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-material-theming-masterclass';
+export class AppComponent implements OnInit {
+  @HostBinding('class')
+  currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
+
+  constructor(private overlayContainer: OverlayContainer) {}
+
+  ngOnInit(): void {
+    this.setTheme('dark-theme');
+  }
+
+  onSelectionChange(change: MatSelectChange): void {
+    this.setTheme(change.source.value);
+  }
+
+  setTheme(currentTheme: 'light-theme' | 'dark-theme'): void {
+    this.currentTheme = currentTheme;
+    this.overlayContainer
+      .getContainerElement()
+      .classList.remove(
+        this.currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme'
+      );
+    this.overlayContainer
+      .getContainerElement()
+      .classList.add(this.currentTheme);
+  }
 }
