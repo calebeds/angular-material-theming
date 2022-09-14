a4884c8b5bdb66618db6c6f7e2309f2ceb5fcc1a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-09-13 23:14:29 -0300

    lazy load the light and dark themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
-import { OverlayContainer } from '@angular/cdk/overlay';
 import { DOCUMENT } from '@angular/common';
-import {
-  Component,
-  HostBinding,
-  Inject,
-  OnInit,
-  Renderer2,
-} from '@angular/core';
+import { Component, Inject, Renderer2 } from '@angular/core';
 import { MatSelectChange } from '@angular/material/select';
 
 @Component({
@@ -14,25 +7,19 @@ import { MatSelectChange } from '@angular/material/select';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  readonly themeAnchor = this.document.getElementById('app-theme');
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2
   ) {}
 
-  ngOnInit(): void {
-    this.setTheme('dark-theme');
-  }
-
-  onSelectionChange(change: MatSelectChange): void {
-    this.setTheme(change.source.value);
-  }
-
-  setTheme(currentTheme: 'light-theme' | 'dark-theme'): void {
-    this.renderer.removeClass(
-      this.document.body,
-      currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme'
+  setTheme(change: MatSelectChange): void {
+    const currentTheme = change.source.value;
+    this.renderer.setAttribute(
+      this.themeAnchor,
+      'href',
+      `/${currentTheme}.css`
     );
-    this.renderer.addClass(this.document.body, currentTheme);
   }
 }
