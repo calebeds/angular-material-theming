84280342c853091f6a4b6f5fdad26a4723d14ce7
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-09-17 01:31:22 -0300

    migrate to angular 12 with old theme api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,9 @@ import { SharedMaterialModule } from './shared-material/shared-material.module';
 import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BannerComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    SharedMaterialModule
-  ],
+  declarations: [AppComponent, BannerComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, SharedMaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
